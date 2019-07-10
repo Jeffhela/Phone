c32803e83897e4fb2589777949fbf6dbc5c32de4
Jeffrey <jeromebro@msn.com>
2019-07-09 20:27:54 -0500

    Add files via upload

---
+++ PhoneTester.java
@@ -0,0 +1,14 @@
+public class PhoneTester {
+    public static void main(String[] args) {
+        Galaxy S9 = new Galaxy("S9", 63, "Verizon", "Ring Ring Ring!");
+        IPhone x = new IPhone("X", 100, "AT&T", "Zing");
+
+        S9.displayInfo();
+        System.out.println(S9.ring());
+        System.out.println(S9.unlock());
+
+        x.displayInfo();
+        System.out.println(x.ring());
+        System.out.println(x.unlock());
+    }
+}
